8eecc61acfe4e773bc50730c6d90a2d7c271f7c4
An-Haeeun-02 <101271701+An-Haeeun-02@users.noreply.github.com>
2024-03-13 06:03:59 +0000

    2주차 과제

--- print.js
+++ print.js
@@ -2,19 +2,24 @@
 "use strict";
 
 // @TODO: 변수를 값이 주세요.
-let charName, 
-    charAge, 
-    charDomain;
+let charName= "Wolverine", 
+    charAge = 300, 
+    charDomain = "Daedpool's house";
 
 // @TODO: 3변수를 보간된 값으로 인쇄하는 함수를 만들어 주세요.
 let printChar = () => {
+    console.log('My name is %s', charName);
+    console.log('I am %d years old', charAge);
+    console.log(`I live in ${charDomain}.`);
+
+
     // console.log 변수로 만든 메시지.
     // %s (string)
     // %d (digit)
     // `${변수}` (템플릿 문자열)
 };
 
-
+printChar()
 
 
 
